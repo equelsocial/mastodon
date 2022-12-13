80c4607a8c30a2c0e855d20eada501963946887c
aarni <aarni.alasaarela@gmail.com>
2022-12-13 13:55:47 +0200

    explore page auth button styling and make them redirect to oidc endpoint

--- app/javascript/mastodon/features/ui/components/header.js
+++ app/javascript/mastodon/features/ui/components/header.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Logo from 'mastodon/components/logo';
 import { Link, withRouter } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
-import { registrationsOpen, me } from 'mastodon/initial_state';
+import { me } from 'mastodon/initial_state';
 import Avatar from 'mastodon/components/avatar';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { openModal } from 'mastodon/actions/modal';
+import Icon from 'mastodon/components/icon';
 
 const Account = connect(state => ({
   account: state.getIn(['accounts', me]),
@@ -16,14 +16,7 @@ const Account = connect(state => ({
   </Link>
 ));
 
-const mapDispatchToProps = (dispatch) => ({
-  openClosedRegistrationsModal() {
-    dispatch(openModal('CLOSED_REGISTRATIONS'));
-  },
-});
-
-export default @connect(null, mapDispatchToProps)
-@withRouter
+export default @withRouter
 class Header extends React.PureComponent {
 
   static contextTypes = {
@@ -31,13 +24,12 @@ class Header extends React.PureComponent {
   };
 
   static propTypes = {
-    openClosedRegistrationsModal: PropTypes.func,
     location: PropTypes.object,
   };
 
   render () {
     const { signedIn } = this.context.identity;
-    const { location, openClosedRegistrationsModal } = this.props;
+    const { location } = this.props;
 
     let content;
 
@@ -49,26 +41,17 @@ class Header extends React.PureComponent {
         </>
       );
     } else {
-      let signupButton;
-
-      if (registrationsOpen) {
-        signupButton = (
-          <a href='/auth/sign_up' className='button button-tertiary'>
-            <FormattedMessage id='sign_in_banner.create_account' defaultMessage='Create account' />
-          </a>
-        );
-      } else {
-        signupButton = (
-          <button className='button button-tertiary' onClick={openClosedRegistrationsModal}>
-            <FormattedMessage id='sign_in_banner.create_account' defaultMessage='Create account' />
-          </button>
-        );
-      }
 
       content = (
         <>
-          <a href='/auth/sign_in' className='button'><FormattedMessage id='sign_in_banner.sign_in' defaultMessage='Sign in' /></a>
-          {signupButton}
+          <a href='/auth/auth/openid_connect' className='button button-with-icon'>
+            <Icon id="linkedin" fixedWidth aria-hidden='true' />
+            <FormattedMessage id='sign_in_banner.sign_in' defaultMessage='Log in' />
+          </a>
+          <a href='/auth/auth/openid_connect' className='button button-tertiary button-with-icon'>
+            <Icon id="linkedin" fixedWidth aria-hidden='true' />
+            <FormattedMessage id='sign_in_banner.create_account' defaultMessage='Join now' />
+          </a>
         </>
       );
     }
--- app/javascript/mastodon/features/ui/components/sign_in_banner.js
+++ app/javascript/mastodon/features/ui/components/sign_in_banner.js
@@ -1,38 +1,19 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import { useDispatch } from 'react-redux';
-import { registrationsOpen } from 'mastodon/initial_state';
-import { openModal } from 'mastodon/actions/modal';
+import Icon from 'mastodon/components/icon';
 
 const SignInBanner = () => {
-  const dispatch = useDispatch();
-
-  const openClosedRegistrationsModal = useCallback(
-    () => dispatch(openModal('CLOSED_REGISTRATIONS')),
-    [dispatch],
-  );
-
-  let signupButton;
-
-  if (registrationsOpen) {
-    signupButton = (
-      <a href='/auth/sign_up' className='button button--block button-tertiary'>
-        <FormattedMessage id='sign_in_banner.create_account' defaultMessage='Create account' />
-      </a>
-    );
-  } else {
-    signupButton = (
-      <button className='button button--block button-tertiary' onClick={openClosedRegistrationsModal}>
-        <FormattedMessage id='sign_in_banner.create_account' defaultMessage='Create account' />
-      </button>
-    );
-  }
-
   return (
     <div className='sign-in-banner'>
-      <p><FormattedMessage id='sign_in_banner.text' defaultMessage='Sign in to follow profiles or hashtags, favourite, share and reply to posts, or interact from your account on a different server.' /></p>
-      <a href='/auth/sign_in' className='button button--block'><FormattedMessage id='sign_in_banner.sign_in' defaultMessage='Sign in' /></a>
-      {signupButton}
+      <p><FormattedMessage id='sign_in_banner.text' defaultMessage='For a safe community, Equel members must use LinkedIn to sign in with their real identities.' /></p>
+      <a href='/auth/auth/openid_connect' className='button button--block button-with-icon'>
+        <Icon id="linkedin" fixedWidth aria-hidden='true' />
+        <FormattedMessage id='sign_in_banner.sign_in' defaultMessage='Log in' />
+      </a>
+      <a href='/auth/auth/openid_connect' className='button button--block button-tertiary button-with-icon'>
+        <Icon id="linkedin" fixedWidth aria-hidden='true' />
+        <FormattedMessage id='sign_in_banner.create_account' defaultMessage='Join now' />
+      </a>
     </div>
   );
 };
