36dc109530c12f8afef60cf9dfbdb96a05e67850
aarni <aarni.alasaarela@gmail.com>
2023-01-17 16:43:48 +0200

    remove footer from auth page and add change button text to 'please wait...' after click

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -84,7 +84,7 @@ module ApplicationHelper
 
   def provider_sign_in_link(provider)
     label = Devise.omniauth_configs[provider]&.strategy&.display_name.presence || I18n.t("auth.providers.#{provider}", default: provider.to_s.chomp('_oauth2').capitalize)
-    link_to label, omniauth_authorize_path(:user, provider), class: "button button-#{provider}", method: :post
+    link_to label, omniauth_authorize_path(:user, provider), class: "button button-#{provider}", method: :post, data: { disable_with: "Please wait..." }
   end
 
   def locale_direction
